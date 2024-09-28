5c15834e943645d03fbc00d68d26c663fdc1f776
Ayyoub Moqine <122099054+aymoqine@users.noreply.github.com>
2024-09-28 02:07:12 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+void main() {
+  // The name of all variables
+  var userName, profileFollowers, profileFollowing, profileBio, watchHours;
+  // The variables data
+  userName = 'aymoqine';
+  profileFollowers = 200;
+  profileFollowing = 2;
+  profileBio = '''UI/UX Designer 
+  Flutter app developer''';
+  watchHours = 5.8;
+  // The output
+  print('''  Your username is : $userName
+  ************************************
+  You have $profileFollowers followers
+  ************************************
+  You follow $profileFollowing
+  ************************************
+  $profileBio
+  ************************************
+  You have $watchHours watch hours
+  ''');
+}
